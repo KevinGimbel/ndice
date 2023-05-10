90dd63e1990c3592960336867d2b3985eda0ee3a
Kevin Gimbel <kevin@gimbel.dev>
2023-05-10 09:48:10 +0200

    refactor: put structs in src/structs.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,19 @@
 use regex::Regex;
 
-pub mod ndice;
+pub mod structs;
 
-use ndice::structs::{RollingHand, RollingHandResult, Dice, DiceResult};
+use structs::{Dice, DiceModifier, DiceResult, RollingHand, RollingHandResult};
 
-const DICE_REGEX: &str = r#"(?P<numberOfDice>[^[dw\s]]+\d?)(?P<diceIndicator>[dw])?(?P<numberOfSides>\d+)"#;
+const DICE_REGEX: &str = r#"(?P<numberOfDice>[^[dwe\s+-]]+\d?)(?P<explodingDice>[e])?(?P<diceIndicator>[dw])(?P<numberOfSides>\d+)(?P<mod>[+-]\d+)?"#;
 
-fn str_to_u64(input: &str) -> u64 {
-    let du64: u64 = input.parse().unwrap();
-    return du64;
+fn str_to_i64(input: &str) -> i64 {
+    let di64: i64 = input.parse().unwrap();
+    return di64;
 }
 
 fn parse_dice_from_str(input: &str) -> Option<regex::Captures<'_>> {
     let rx = Regex::new(DICE_REGEX).unwrap();
-    
+
     rx.captures(input)
 }
 
@@ -25,21 +25,45 @@ pub fn parse_dices(args: Vec<String>) -> RollingHand {
 
         match res {
             Some(matches) => {
-                let mut i: u64 = 0;
+                let mut i: i64 = 0;
                 let number_of_dice = matches.name("numberOfDice").unwrap().as_str();
                 let number_of_sides = matches.name("numberOfSides").unwrap().as_str();
-                let dices = str_to_u64(number_of_dice);
-                let sides = str_to_u64(number_of_sides);
+                let exploding_dice = match matches.name("explodingDice") {
+                    Some(_ed) => true,
+                    None => false,
+                };
+                let modifier = match matches.name("mod") {
+                    Some(ed) => ed.as_str(),
+                    None => "",
+                };
+                let modifier_value: i64 =
+                    match modifier.split(&['+', '-']).collect::<Vec<&str>>().last() {
+                        Some(v) => match v.parse::<i64>() {
+                            Ok(v) => v,
+                            Err(_e) => 0,
+                        },
+                        _ => 0,
+                    };
+                let dices = str_to_i64(number_of_dice);
+                let sides = str_to_i64(number_of_sides);
                 // match minimum number on dice based on dice type
-                let min: u64 = match sides {
+                let min: i64 = match sides {
                     10 => 0,
-                    _ => 1
+                    _ => 1,
                 };
                 while i < dices {
-                    i = i + 1;  
-                    hand.dices.push(Dice{min, sides})
+                    i = i + 1;
+                    hand.dices.push(Dice {
+                        min,
+                        sides,
+                        modifier: DiceModifier {
+                            is_plus: modifier.contains("+"),
+                            value: modifier_value,
+                        },
+                        exploding_dice,
+                    })
                 }
-            },
+            }
             _ => {}
         }
     }
@@ -53,9 +77,113 @@ pub fn roll_dices(in_hand: RollingHand) -> RollingHandResult {
 
     for dice in hand.dices {
         let dice_result = dice.roll();
-        result.rolls.push(DiceResult{sides: dice.sides, result: dice_result});
+        let mut modi = "-";
+        if dice.modifier.is_plus {
+            modi = "+";
+        }
+        result.rolls.push(DiceResult {
+            sides: dice.sides,
+            result: dice_result,
+            modifier: format!("{}{}", modi, dice.modifier.value),
+        });
         result.sum += dice_result;
+        if dice.modifier.is_plus {
+            result.sum += dice.modifier.value
+        } else {
+            result.sum -= dice.modifier.value
+        }
     }
 
     result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_dice() {
+        struct TestCase<'a> {
+            name: &'a str,
+            input: &'a str,
+            expected: RollingHand,
+        }
+
+        let tests = [
+            TestCase {
+                name: "dice: 1d4",
+                input: "1d4",
+                expected: RollingHand {
+                    dices: vec![Dice {
+                        sides: 4,
+                        min: 1,
+                        ..Default::default()
+                    }],
+                    ..Default::default()
+                },
+            },
+            TestCase {
+                name: "dice: 3d12",
+                input: "3d12",
+                expected: RollingHand {
+                    dices: vec![
+                        Dice {
+                            sides: 12,
+                            ..Default::default()
+                        },
+                        Dice {
+                            sides: 12,
+                            ..Default::default()
+                        },
+                        Dice {
+                            sides: 12,
+                            ..Default::default()
+                        },
+                    ],
+                    ..Default::default()
+                },
+            },
+            TestCase {
+                name: "dice: 1ed6",
+                input: "1ed6",
+                expected: RollingHand {
+                    dices: vec![Dice {
+                        sides: 6,
+                        exploding_dice: true,
+                        ..Default::default()
+                    }],
+                    ..Default::default()
+                },
+            },
+            TestCase {
+                name: "dice: 1w6",
+                input: "1w6",
+                expected: RollingHand {
+                    dices: vec![Dice {
+                        sides: 6,
+                        ..Default::default()
+                    }],
+                    ..Default::default()
+                },
+            },
+            TestCase {
+                name: "dice: 1d10",
+                input: "1d10",
+                expected: RollingHand {
+                    dices: vec![Dice {
+                        sides: 10,
+                        min: 0,
+                        ..Default::default()
+                    }],
+                    ..Default::default()
+                },
+            },
+        ];
+
+        for test in tests {
+            dbg!(test.name);
+            let dice = parse_dices(vec![String::from(test.input)]);
+            assert_eq!(test.expected, dice)
+        }
+    }
 }
---
+++ src/structs.rs
@@ -0,0 +1,109 @@
+use rand::{thread_rng, Rng};
+#[cfg(feature = "json")]
+use serde::{Deserialize, Serialize};
+use std::fmt;
+
+#[derive(Debug)]
+#[cfg_attr(feature = "json", derive(Serialize, Deserialize))]
+pub struct Dice {
+    pub sides: i64,
+    pub min: i64,
+    pub modifier: DiceModifier,
+    pub exploding_dice: bool,
+}
+
+impl Dice {
+    pub fn roll(&self) -> i64 {
+        let mut rng = thread_rng();
+        let n: i64 = rng.gen_range(self.min..self.sides);
+        n
+    }
+}
+
+impl Default for Dice {
+    fn default() -> Self {
+        Self {
+            sides: 0,
+            min: 1,
+            modifier: DiceModifier {
+                is_plus: false,
+                value: 0,
+            },
+            exploding_dice: false,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct DiceModifier {
+    pub is_plus: bool,
+    pub value: i64,
+}
+
+#[derive(Debug)]
+#[cfg_attr(feature = "json", derive(Serialize, Deserialize))]
+pub struct RollingHand {
+    pub dices: Vec<Dice>,
+    pub result: RollingHandResult,
+}
+
+impl PartialEq for RollingHand {
+    fn eq(&self, other: &Self) -> bool {
+        // TODO: maybe this isn't the best implementation 😬
+        return format!("{:?}", self.dices) == format!("{:?}", other.dices);
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        // TODO: maybe this isn't the best implementation 😬
+        return format!("{:?}", self.dices) != format!("{:?}", other.dices);
+    }
+}
+
+#[derive(Debug)]
+#[cfg_attr(feature = "json", derive(Serialize, Deserialize))]
+pub struct RollingHandResult {
+    pub sum: i64,
+    pub rolls: Vec<DiceResult>,
+}
+
+impl Default for RollingHandResult {
+    fn default() -> Self {
+        Self {
+            sum: 0,
+            rolls: vec![],
+        }
+    }
+}
+
+#[derive(Debug)]
+#[cfg_attr(feature = "json", derive(Serialize, Deserialize))]
+pub struct DiceResult {
+    pub sides: i64,
+    pub result: i64,
+    pub modifier: String,
+}
+
+impl fmt::Display for DiceResult {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "d{} => {} ({})", self.sides, self.result, self.modifier)
+    }
+}
+
+impl fmt::Display for RollingHandResult {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut rolls: Vec<String> = vec![];
+        for dice in &self.rolls {
+            rolls.push(format!("{}", dice));
+        }
+        write!(f, "Rolls: {:?}\nResult: {}", rolls, self.sum)
+    }
+}
+
+impl Default for RollingHand {
+    fn default() -> Self {
+        Self {
+            dices: vec![],
+            result: RollingHandResult::default(),
+        }
+    }
+}
